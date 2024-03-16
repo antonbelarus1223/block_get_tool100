eb09dd7404deb6d3f3c36179c4066fade0921b4e
antonbelarus1223 <38377021+antonbelarus1223@users.noreply.github.com>
2024-03-16 10:39:35 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+    "name": "block_get_tool100",
+    "version": "1.0.0",
+    "description": "A block tools set for working with Web3 and Ethereum smart contracts.",
+    "main": "index.js",
+    "scripts": {
+      "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "keywords": ["web3", "ethereum", "smart contract"],
+    "author": "antonbelarus1223",
+    "license": "MIT",
+    "dependencies": {
+      "web3": "^1.5.0"
+    },
+    "repository": {
+      "type": "git",
+      "url": "https://github.com/antonbelarus1223/block_get_tool100.git"
+    }
+  }
+  
